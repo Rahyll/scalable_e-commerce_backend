5027aa5425dc76a44fda7f4bd7a173d94e5da445
Rahyl <rahylreo@gmail.com>
2024-11-07 01:18:00 +0530

    attached router for path auth and users ,include body-parser and urlencoder middleware

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.routes.js";
+import usersRouter from "./routes/user.routes.js";
+import bodyParser from "body-parser";
 
 const app = express();
+const databaseUri = `mongodb://localhost:27017/e_commerce`;
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  try {
-    res.status(200).json({
-      message: "website working",
-    });
-  } catch (err) {
-    res.json({
-      message: "Something went wrong",
-    });
-  }
+await mongoose.connect(databaseUri).then(() => {
+  console.log("DataBase Connection successfull");
 });
+
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log("Connection Successfull");
 });
+
+// import pg from "pg";
+
+// const { Client } = pg;
+// const config = {
+//   host: "localhost",
+//   post: 5432,
+//   password: "Rahyl@124",
+// };
+
+// const client = new Client(config);
+// client.connect(() => {
+//   console.log("Database connection successfull");
+// });
