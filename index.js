3733b86209586a1167833f3414d4f4feebd6d2de
Rahyl <rahilreo@gmail.com>
2024-12-01 02:33:55 +0530

    I added the index router file to the main application file to route incoming requests through it. I also imported and called the connectDB function for database connection from its separate file. Additionally, I implemented a new middleware to handle errors.

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 import express from "express";
-import mongoose from "mongoose";
-import authRouter from "./routes/auth.routes.js";
-import usersRouter from "./routes/user.routes.js";
+// import multer, { memoryStorage } from "multer";
+import connectDB from "./db.js";
+import apiRouter from "./routes/index.routes.js";
 import bodyParser from "body-parser";
 import "dotenv/config";
+import errorMiddleware from "./middlewares/error.middleware.js";
+// import multerErrorMiddleware from "./middlewares/multerError.middleware.js";
 
-const app = express();
-const databaseUri = `mongodb://localhost:27017/e_commerce`;
+const app = express(); // Initialize the Express app
+
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
-const port = 3000;
+// Multer configuration for parsing multipart/form-data
+// const upload = multer({ storage: memoryStorage() });
+// app.use(upload.any());
 
-await mongoose.connect(databaseUri).then(() => {
-  console.log("DataBase Connection successfull");
+// Connect to databases
+connectDB();
+
+//Routes
+app.use("/api", apiRouter);
+
+// app.use(multerErrorMiddleware);
+app.use(errorMiddleware);
+
+// Default route for any undefined routes
+app.all("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
 });
 
-app.use("/auth", authRouter);
-app.use("/users", usersRouter);
-
-app.listen(port, () => {
-  console.log("Connection Successfull");
+//Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
