b7e5732ada811300ae6271a1b2e5de7097afe546
Rahyl <rahilreo@gmail.com>
2024-12-10 01:15:10 +0530

    feat: update db.js for MongoDB Atlas; refactor S3 config for environment-based keys and region

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
-const databaseUri = `mongodb://localhost:27017/e_commerce`;
+import "dotenv/config";
+const databaseUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@development.dlozu.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Development`;
 
 export default async () => {
   try {
