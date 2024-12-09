b6b3dcc92819e2debae4bc5c685dd87ae7b99394
Rahyl <rahilreo@gmail.com>
2024-12-10 01:16:34 +0530

    fix: handle Multer errors with specific 400 status code

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,5 +1,10 @@
+import multer from "multer";
 export default (err, req, res, next) => {
-  let statusCode = err.statusCode || 500;
+  let statusCode = err.statusCode
+    ? err.statusCode
+    : err instanceof multer.MulterError
+    ? 400
+    : 500;
   let errorMessage = err.message || "Internal Server Error";
 
   res.status(statusCode).json({
